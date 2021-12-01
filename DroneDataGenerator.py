c4fc1478cfc578fd006a5d5965ec4e75d690b0da
Kody <71484999+Koratun@users.noreply.github.com>
2021-12-01 11:28:56 -0700

    Manual upload of data to correct errored branch

--- DroneDataGenerator.py
+++ DroneDataGenerator.py
@@ -6,8 +6,10 @@ import random
 import os
 
 
+datadir = "Data"
+
 class DroneDataGenerator(tf.keras.utils.Sequence):
-    def __init__(self, datadir, batch_size=32, shuffle=True):
+    def __init__(self, batch_size=32, shuffle=True):
         # Initialization
         self.datdir = datadir
         # Get a list of runs available in the data directory
@@ -17,12 +19,35 @@ class DroneDataGenerator(tf.keras.utils.Sequence):
         self.batch_size = batch_size
         self.shuffle = shuffle
         
-        # read Y positions
+        # read positional data
+        xData = []
+        for run in self.run_dirs:
+            with open(os.path.join(run, 'xPos.txt'), 'r') as f:
+                xData += [float(s) for s in f.readlines()]
+        self.xData = np.array(xData)
+
         yData = []
         for run in self.run_dirs:
             with open(os.path.join(run, 'yPos.txt'), 'r') as f:
                 yData += [float(s) for s in f.readlines()]
         self.yData = np.array(yData)
+
+        zData = []
+        for run in self.run_dirs:
+            with open(os.path.join(run, 'zPos.txt'), 'r') as f:
+                zData += [float(s) for s in f.readlines()]
+        self.zData = np.array(zData)
+
+        # Read rotational data and calculate their sines and cosines
+        rData = []
+        for run in self.run_dirs:
+            with open(os.path.join(run, 'rotation.txt'), 'r') as f:
+                rData += [[[np.cos(float(r)), np.sin(float(r))] for r in s.split(', ')] for s in f.readlines()]
+        self.rData = np.array(rData)
+        # Shape will be:
+        # [[[cos(x), sin(x)], [cos(y), sin(y)], [cos(z), sin(z)]], [and so on...]]
+        # [image, xyz, cos/sine]
+
         #print(self.yData.shape)
 
         self.photofiles = glob.glob(os.path.join(datadir, "Run*", 'photos', '*.png'))
@@ -55,27 +80,30 @@ class DroneDataGenerator(tf.keras.utils.Sequence):
         # Generate indexes of the batch
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
         
-        ypos = np.zeros((self.batch_size,), dtype='float32')
+        pos = np.zeros((self.batch_size, 3), dtype='float32')
         image_concat_pair_batch = np.zeros((self.batch_size, 2, self.xsize, self.ysize, 3), dtype='float32')
-        
+        rotation_difference = np.zeros((self.batch_size, 3, 2), dtype='float32')
+
         for count, ind in enumerate(indexes):
             # get images
             curr_image = imageio.imread(self.photofiles[ind])
             next_image = imageio.imread(self.photofiles[ind+1])
             # get positions
-            curr_ypos = self.yData[ind]
-            new_ypos = self.yData[ind+1]
+            curr_pos = np.array([self.xData[ind], self.yData[ind], self.zData[ind]])
+            new_pos = np.array([self.xData[ind+1], self.yData[ind+1], self.zData[ind+1]])
             # diff
-            ypos[count] = new_ypos-curr_ypos
+            pos[count] = new_pos-curr_pos
+            # get rotations
+            rotation_difference[count] = self.rData[ind+1] - self.rData[ind]
             # expand axis
-            curr_image = np.array(curr_image)[np.newaxis, :, :, :-1]
-            next_image = np.array(next_image)[np.newaxis, :, :, :-1]
+            curr_image = np.array(curr_image)[np.newaxis, :, :, :3]
+            next_image = np.array(next_image)[np.newaxis, :, :, :3]
             # concatenat image pairs
             image_concat = np.concatenate((curr_image, next_image), axis=0)/255.
             # store
             image_concat_pair_batch[count] = image_concat
         
-        return tf.convert_to_tensor(image_concat_pair_batch, dtype=tf.dtypes.float32), tf.convert_to_tensor(ypos, dtype=tf.dtypes.float32)
+        return tf.convert_to_tensor(image_concat_pair_batch, dtype=tf.dtypes.float32), tf.convert_to_tensor(rotation_difference, dtype=tf.dtypes.float32), tf.convert_to_tensor(pos, dtype=tf.dtypes.float32)
 
     def on_epoch_end(self):
         if self.shuffle == True:
--- deep_sight.py
+++ deep_sight.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, optimizers
 import DroneDataGenerator as gen
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 # # Deprecated function. tf Datasets are better.
 # def preprocess_data():
@@ -112,6 +115,10 @@ def main():
     image = layers.BatchNormalization()(image)
     image = layers.Dropout(.3)(image)
 
+    # Reduce feature map to one channel in the last dimension.
+    image = layers.Conv2D(1, (3, 3))(image)
+    image = layers.LeakyReLU()(image)
+
     image_model = keras.Model(single_image_input, image)
     
     # Create larger model
@@ -123,35 +130,117 @@ def main():
     image_outputs = [image_model(first_image), image_model(second_image)]
     model = layers.Concatenate()(image_outputs)
 
-    
-
+    # Use the two image-shaped feature maps in a convolutional layer to create a new feature map.
+    # This should detect the movement of features between the two images.
+    model = layers.Conv2D(64, (3, 3))(model)
+    model = layers.LeakyReLU()(model)
+    model = layers.BatchNormalization()(model)
+    model = layers.Dropout(.3)(model)
 
     model = layers.Flatten()(model)
+
+    # Input the rotational data and expand it to the same shape as the feature map.
+    rotational_input = keras.Input(shape=(3,2))
+    # Flatten the input
+    rotation_portion = layers.Flatten()(rotational_input)
+
+    # Run through dense layer
+    rotation_portion = layers.Dense(64)(rotation_portion)
+    rotation_portion = layers.LeakyReLU()(rotation_portion)
+    rotation_portion = layers.BatchNormalization()(rotation_portion)
+    rotation_portion = layers.Dropout(.2)(rotation_portion)
+
+    model = layers.Concatenate()([model, rotation_portion])
 
     model = layers.Dense(128)(model)
     model = layers.LeakyReLU()(model)
     model = layers.BatchNormalization()(model)
     model = layers.Dropout(.3)(model)
 
-    # Output is change in y-position of drone
-    out_layer = layers.Dense(1, activation='linear')(model)
+    # Output is change in position of drone
+    out_layer = layers.Dense(3, activation='linear')(model)
 
-    final_model = keras.Model(image_inputs, out_layer)
+    final_model = keras.Model([image_inputs, rotational_input], out_layer)
     final_model.compile(loss="mse", optimizer=optimizers.Adam(lr=0.0003, beta_1=0.7))
 
     image_model.summary()
 
     final_model.summary()
 
+    return final_model
 
-    #Preprocess data
-    print("Loading and processing data...")
-    train_data_generator = gen.DroneDataGenerator("Data")
 
-    #Train model
-    final_model.fit(generator=train_data_generator, use_multiprocessing=True, epochs=5, workers=4)
+    
 
+
+# Plots the history of the training algorithm
+def plot_losses(history):
+    sns.set()  # Switch to the Seaborn look
+    plt.plot(history.history['loss'], label='Training set',
+             color='blue', linestyle='-')
+    plt.xlabel("Epochs", fontsize=30)
+    plt.ylabel("Loss", fontsize=30)
+    plt.xlim(0, len(history.history['loss']))
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=30)
+    plt.show()
+
+
+# Tests the model on a single input from the train generator
+def test_model(model, train_gen: tf.keras.utils.Sequence):
+    # Grab the first image from the train generator
+    image_data, rot, posData = train_gen[0]
+    # Run the model on the image
+    prediction = model.predict([image_data, rot])
+    # Get the average error between the prediction and the actual position
+    error = np.average(np.abs(prediction - posData.numpy()))
+    print("Average error over 32 samples:", error)
+    # Print the predicted data and actual data for one sample
+    print("Predicted:", prediction[0])
+    print("Actual:", posData.numpy()[0])
 
 
 if __name__ == "__main__":
-    main()
+    model = main()
+    #model = keras.models.load_model("D:\\Documents\\Programming\\Night Tours\\DroneMovement")
+
+    #Preprocess data
+    print("Loading and processing data...")
+    gen.datadir = "Data"
+    train_data_generator = gen.DroneDataGenerator(batch_size=32)
+
+    # Convert from keras sequence to tf.data.Dataset
+    tfgen = tf.data.Dataset.from_generator(lambda: range(len(train_data_generator)), tf.uint8)
+    tfgen = tfgen.shuffle(buffer_size=len(train_data_generator), reshuffle_each_iteration=True)
+    tfgen = tfgen.map(lambda i: tf.py_function(train_data_generator.__getitem__, inp=[i], Tout=(
+        tf.float32, tf.float32, tf.float32)), 
+        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    # Reshape the data to the proper shape
+    tfgen = tfgen.map(lambda image, rot, pos: ((image, rot), pos), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    # tfgen = tf.data.Dataset.from_generator(gen.DroneDataGenerator, args=[32], output_signature=(
+    #     (tf.TensorSpec(shape=(None, 2, 128, 128, 3), dtype=tf.float32),
+    #     tf.TensorSpec(shape=(None, 3, 2), dtype=tf.float32)),
+    #     tf.TensorSpec(shape=(None, 3), dtype=tf.float32)
+    # ))
+    
+    # # autotune the number of samples that are prefetched
+    tfgen = tfgen.prefetch(tf.data.experimental.AUTOTUNE)
+    tfgen = tfgen.repeat()
+
+    # print the data from one sample from the generator
+    # print(tfgen.take(1))
+
+    callbacks = [keras.callbacks.TensorBoard(log_dir='logs', histogram_freq=1, write_graph=True, write_images=True, profile_batch=(20, 40))]
+
+    #Train model
+    history = model.fit(tfgen, steps_per_epoch=len(train_data_generator), epochs=2, callbacks=callbacks)
+    
+    test_model(model, train_data_generator)
+
+    #model.save("D:\\Documents\\Programming\\Night Tours\\DroneMovement")
+
+    # Not helpful when you only have 1 epoch
+    #plot_losses(history)
